e58f7794e0133fd26e17cdd5c2db9051bbc51971
joaocmd <joaodavid1999@hotmail.com>
2021-05-27 19:03:44 +0100

    Start of instance methods
    Co-authored-by: André Breda <andrebreda@tecnico.ulisboa.pt>

--- round2.jl
+++ round2.jl
@@ -1,24 +1,42 @@
 import Pkg
-Pkg.add("JavaCall")
+# Pkg.add("JavaCall")
 
 ##
 
 using JavaCall
 
 JavaCall.init(["-Xmx128M"])
+Base.show(io::IO, obj::JavaCall.JavaObject) =
+    print(io, jcall(obj, "toString", JString, ()))
+
+struct JavaValue
+    ref::JavaCall.JavaObject 
+    # maybe try to use a NamedTuple?
+    methods::NamedTuple
+end
+
+Base.show(io::IO, jv::JavaValue) =
+    show(io, getfield(jv, :ref))
+
+Base.getproperty(jv::JavaValue, sym::Symbol) =
+    getfield(jv, :methods)[sym](getfield(jv, :ref))
 
 ##
 
 macro pimport(package)
     local p = @jimport $package
-
 end
-
 
 @pimport java.lang.Math
 
+mdf = @jimport java.lang.reflect.Modifier
+isstatic(method::JMethod) = begin
+    modifiers = jcall(method, "getModifiers", jint, ())
+    Bool(jcall(mdf, "isStatic", jboolean, (jint,), modifiers))
+end
+
 ##
-normalizeJavaType(name::Union{Core.String, Base.SubString}) = begin
+normalizeJavaType(name::Union{Core.String,Base.SubString}) = begin
     if name == "short" || name == "java.lang.Short"
         JavaCall.jshort
     elseif name == "int" || name == "java.lang.Integer"
@@ -40,7 +58,7 @@ normalizeJavaType(name::Union{Core.String, Base.SubString}) = begin
     elseif (m = Base.match(r"[^\][]+((\[])+)", name); typeof(m) != Nothing)
         n = m.captures[1].ncodeunits ÷ 2
         name_len = length(name) - m.captures[1].ncodeunits
-        Array{normalizeJavaType(Base.SubString(name, 1:name_len)), n}
+        Array{normalizeJavaType(Base.SubString(name, 1:name_len)),n}
     else
         # TODO: wrapper? maybe?
         JavaCall.JavaObject{Symbol(name)}
@@ -50,26 +68,37 @@ end
 normalizeJavaType(x::JavaCall.JClass) = begin
     normalizeJavaType(getname(x))
 end
-
+    
 ##
 
-jlm = @jimport java.lang.Math
-methodNames = []
+jlm = @jimport java.time.LocalDate
+methodnames = []
+instancemethods = []
+
+PROBLEM = Nothing
 
 for method in JavaCall.listmethods(jlm)
     name = getname(method)
     hygienic_name = "pava2_" * name
     returntype = normalizeJavaType(getreturntype(method))
     parametertypes = Tuple(normalizeJavaType(p) for p in getparametertypes(method))
-    nrparams = length(parametertypes)
-    parameters = [Symbol("p" * string(i)) for i in 1:nrparams]
+    parameters = [Symbol("p" * string(i)) for i in 1:length(parametertypes)]
 
     typed_parameters = [:($(a[1])::$(a[2])) for a in zip(parameters, parametertypes)]
 
-    methodNames = push!(methodNames, getname(method))
-    newMethod = :( $(Symbol(hygienic_name))($(typed_parameters...)) = jcall(jlm, $name, $returntype, $parametertypes, $(parameters...)) )
-    # dump(newMethod)
-    eval(newMethod)
+    # TODO: in these functions, if the returned is an object, wrap it in the proxy
+    # Same applies if a proxy is passed as function argument, will probably need some changes to the static
+    # methods to allow that
+    if (isstatic(method))
+        push!(methodnames, getname(method))
+        newmethod = :( $(Symbol(hygienic_name))($(typed_parameters...)) = jcall(jlm, $name, $returntype, $parametertypes, $(parameters...)) )
+        eval(newmethod)
+    else
+        push!(instancemethods, getname(method))
+        inst_param = :(inst::$(jlm))
+        newmethod = :( $(Symbol(hygienic_name))($inst_param) = ($(typed_parameters...),) -> jcall(inst, $name, $returntype, $parametertypes, $(parameters...)))
+        eval(newmethod)
+    end
 end
 
 fields = []
@@ -79,12 +108,20 @@ for field in JavaCall.listfields(jlm)
     type = normalizeJavaType(type)
 
     # HACK: fields may not be final, it's not desirable to be stuck with the value currently present
+    # val = () -> JavaCall.jfield(jlm, name, type) something like this would be good but not really this??
     val = JavaCall.jfield(jlm, name, type)
     fields = push!(fields, (Symbol(name), val))
 end
 
-unique!(methodNames) # maybe use a set?
-methods_ = zip([Symbol(m) for m in methodNames], [eval(Symbol("pava2_" * m)) for m in methodNames])
-Math = merge((;fields...), (;methods_...))
+unique!(methodnames) # maybe use a set?
+methods_ = zip([Symbol(m) for m in methodnames], [eval(Symbol("pava2_" * m)) for m in methodnames])
+LocalDate = merge((;fields...), (;methods_...))
 
-##
+unique!(instancemethods)
+instancemethods_ = (;zip([Symbol(m) for m in instancemethods], [eval(Symbol("pava2_" * m)) for m in instancemethods])...)
+
+now = LocalDate.now()
+method = (plusDays = (jtld) -> (days) -> jcall(jtld, "plusDays", jlm, (jlong,), days),)
+
+a = JavaValue(now, instancemethods_)
+#
