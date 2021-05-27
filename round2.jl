dbe8a6b39cabf70b6357a0469f611393318f3b2c
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-05-27 13:05:34 +0100

    Support (static) method overloading, load static fields
    Co-authored-by: João David <joaodavid@tecnico.ulisboa.pt>

--- round2.jl
+++ round2.jl
@@ -1,38 +1,90 @@
 import Pkg
 Pkg.add("JavaCall")
+
+##
+
 using JavaCall
 
 JavaCall.init(["-Xmx128M"])
 
+##
+
 macro pimport(package)
     local p = @jimport $package
-    
+
 end
 
 
 @pimport java.lang.Math
 
+##
+normalizeJavaType(name::Union{Core.String, Base.SubString}) = begin
+    if name == "short" || name == "java.lang.Short"
+        JavaCall.jshort
+    elseif name == "int" || name == "java.lang.Integer"
+        JavaCall.jint
+    elseif name == "long" || name == "java.lang.Long"
+        JavaCall.jlong
+    elseif name == "byte" || name == "java.lang.Byte"
+        JavaCall.jbyte
+    elseif name == "char" || name == "java.lang.Char"
+        JavaCall.jchar
+    elseif name == "float" || name == "java.lang.Float"
+        JavaCall.jfloat
+    elseif name == "double" || name == "java.lang.Double"
+        JavaCall.jdouble
+    elseif name == "boolean" || name == "java.lang.Boolean"
+        JavaCall.jboolean
+    elseif name == "void" || name == "java.lang.Void"
+        JavaCall.jvoid
+    elseif (m = Base.match(r"[^\][]+((\[])+)", name); typeof(m) != Nothing)
+        n = m.captures[1].ncodeunits ÷ 2
+        name_len = length(name) - m.captures[1].ncodeunits
+        Array{normalizeJavaType(Base.SubString(name, 1:name_len)), n}
+    else
+        # TODO: wrapper? maybe?
+        JavaCall.JavaObject{Symbol(name)}
+    end
+end
+
+normalizeJavaType(x::JavaCall.JClass) = begin
+    normalizeJavaType(getname(x))
+end
+
+##
+
 jlm = @jimport java.lang.Math
-methods = listmethods(jlm)
-getreturntype(methods[1])
 methodNames = []
 
-
-for method in methods
+for method in JavaCall.listmethods(jlm)
     name = getname(method)
-    hygienic_name = "pava_" * name
-    returntype = getreturntype(method)
-    parametertypes = getparametertypes(method)
+    hygienic_name = "pava2_" * name
+    returntype = normalizeJavaType(getreturntype(method))
+    parametertypes = Tuple(normalizeJavaType(p) for p in getparametertypes(method))
     nrparams = length(parametertypes)
     parameters = [Symbol("p" * string(i)) for i in 1:nrparams]
 
+    typed_parameters = [:($(a[1])::$(a[2])) for a in zip(parameters, parametertypes)]
+
     methodNames = push!(methodNames, getname(method))
-    # TODO: return types and parameter types
-    # map abs(p1::Int64) to jcall(jint) and abs(p1::Float32) to jcall(jfloat) ? how ?
-    newMethod = :( $(Symbol(hygienic_name))($(parameters...)) = jcall(jlm, $name, jint, (jint,), $(parameters...)) )
-    dump(newMethod)
+    newMethod = :( $(Symbol(hygienic_name))($(typed_parameters...)) = jcall(jlm, $name, $returntype, $parametertypes, $(parameters...)) )
+    # dump(newMethod)
     eval(newMethod)
 end
 
+fields = []
+for field in JavaCall.listfields(jlm)
+    name = JavaCall.getname(field)
+    type = JavaCall.jcall(field, "getType", JClass, ())
+    type = normalizeJavaType(type)
+
+    # HACK: fields may not be final, it's not desirable to be stuck with the value currently present
+    val = JavaCall.jfield(jlm, name, type)
+    fields = push!(fields, (Symbol(name), val))
+end
+
 unique!(methodNames) # maybe use a set?
-Math = (;zip([Symbol(m) for m in methodNames], [eval(Symbol("pava_" * m)) for m in methodNames])...)
+methods_ = zip([Symbol(m) for m in methodNames], [eval(Symbol("pava2_" * m)) for m in methodNames])
+Math = merge((;fields...), (;methods_...))
+
+##
