45680e1cf72adc96b26b3d802b7657038f46ee78
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-06-01 22:35:18 +0100

    MVP
    Missing:
    - Constructors
    - Overloading
    - Ergonomic strings (CharSequence and stuff)
    
    Co-authored-by: João David <joaodavid@tecnico.ulisboa.pt>

--- round4.jl
+++ round4.jl
@@ -5,185 +5,275 @@ Pkg.add("JavaCall")
 using JavaCall
 JavaCall.init(["-Xmx128M"])
 ##
-Base.show(io::IO, obj::JavaCall.JavaObject) =
-    print(io, jcall(obj, "toString", JString, ()))
+module JavaProxyUtils
+    export ImportProxy, InstanceProxy, normalizeJavaType, wrapped, unwrapped
 
-struct JavaValue
-    ref::JavaCall.JavaObject 
-    methods::NamedTuple
-end
+    using JavaCall
 
-struct ClassProxy
-    name::String
-    mod::Module
-end
+    Base.show(io::IO, obj::JavaCall.JavaObject) =
+        print(io, jcall(obj, "toString", JString, ()))
 
-Base.show(io::IO, jv::JavaValue) =
-    show(io, getfield(jv, :ref))
-
-Base.getproperty(jv::JavaValue, sym::Symbol) =
-    getfield(jv, :methods)[sym](getfield(jv, :ref))
-
-    
-Base.show(io::IO, cp::ClassProxy) =
-    show(io, getfield(cp, :name))
-
-Base.getproperty(cp::ClassProxy, sym::Symbol) = begin
-    if string(sym) ∈ getfield(cp, :mod).final
-        eval(:( getfield($cp, :mod).$(sym) )) # get the method/constant
-    else
-        eval(:( getfield($cp, :mod).$sym() )) # call the getter
-    end
-end
-
-Base.setproperty!(cp::ClassProxy, sym::Symbol, v::Any) = begin
-    eval(:( getfield($cp, :mod).$sym($v) ))
-end
-
-##
-
-# public void f(Comparable<?> b)
-# public void f(Serializable c)
-# f(Serializable(o))
-
-mdf = @jimport java.lang.reflect.Modifier
-isstatic(method::Union{JavaCall.JField,JavaCall.JMethod}) = begin
-    modifiers = jcall(method, "getModifiers", jint, ())
-    Bool(jcall(mdf, "isStatic", jboolean, (jint,), modifiers))
-end
-isfinal(method::Union{JavaCall.JField,JavaCall.JMethod}) = begin
-    modifiers = jcall(method, "getModifiers", jint, ())
-    Bool(jcall(mdf, "isFinal", jboolean, (jint,), modifiers))
-end
-
-##
-normalizeJavaType(name::Union{Core.String,Base.SubString}) = begin
-    if name == "short" || name == "java.lang.Short"
-        JavaCall.jshort
-    elseif name == "int" || name == "java.lang.Integer"
-        JavaCall.jint
-    elseif name == "long" || name == "java.lang.Long"
-        JavaCall.jlong
-    elseif name == "byte" || name == "java.lang.Byte"
-        JavaCall.jbyte
-    elseif name == "char" || name == "java.lang.Char"
-        JavaCall.jchar
-    elseif name == "float" || name == "java.lang.Float"
-        JavaCall.jfloat
-    elseif name == "double" || name == "java.lang.Double"
-        JavaCall.jdouble
-    elseif name == "boolean" || name == "java.lang.Boolean"
-        JavaCall.jboolean
-    elseif name == "void" || name == "java.lang.Void"
-        JavaCall.jvoid
-    elseif (m = Base.match(r"[^\][]+((\[])+)", name); typeof(m) != Nothing)
-        n = m.captures[1].ncodeunits ÷ 2
-        name_len = length(name) - m.captures[1].ncodeunits
-        Array{normalizeJavaType(Base.SubString(name, 1:name_len)),n}
-    else
-        # TODO: wrapper? maybe?
-        JavaCall.JavaObject{Symbol(name)}
-    end
-end
-
-normalizeJavaType(x::JavaCall.JClass) = begin
-    normalizeJavaType(getname(x))
-end
-    
-##
-
-# jlm = @jimport java.time.LocalDate
-# methodnames = []
-# instancemethods = []
-
-# module LocalDate
-
-# methodnames = []
-# instancemethodnames = []
-
-function javaClassModuleName(classname::String)
-    "PAvaJavaInterop Class " * classname
-end
-
-function javaClass(classname::String)
-    if ! Base.isdefined(Main, Symbol(javaClassModuleName(classname)))
-        javaClassModule(classname)
+    struct ImportProxy
+        mod::Module
     end
 
-    eval(Symbol(javaClassModuleName(classname)))
-end
+    Base.show(io::IO, proxy::ImportProxy) =
+        print(io, "Java Import " * getfield(proxy, :mod).name)
 
-function javaClassModule(classname::String)
-    class = eval(JavaCall._jimport(classname))
-    mod_name = javaClassModuleName(classname)
-
-    # create module
-    eval(:(module $(Symbol(mod_name)) using JavaCall end))
-    mod = eval(Symbol(mod_name)) 
-
-    for method in JavaCall.listmethods(class)
-        local name = getname(method)
-        local returntype = normalizeJavaType(getreturntype(method))
-        local parametertypes = Tuple(normalizeJavaType(p) for p in getparametertypes(method))
-        local parameters = [Symbol("p" * string(i)) for i in 1:length(parametertypes)]
-        local typed_parameters = [:($(a[1])::$(a[2])) for a in zip(parameters, parametertypes)]
-
-        # TODO: in these functions, if the returned is an object, wrap it in the proxy
-        # Same applies if a proxy is passed as function argument, will probably need some changes to the static
-        # methods to allow that
-        # getvalue(x) = x
-        # getvalue(x::JavaValue) = getfield(x, :ref)
-
-        # foo(bar) -> begin
-        #     parameters = [getvalue(x) for x in parameters]
-        #     res = jcall(jlm, $name, $returntype)
-        #     if typeof(res) == JavaCall.JavaObject
-        #         _instancemethods = @pimport getname(getclass(res)) # how?
-        #         a = JavaValue(res, _instancemethods)
-        #     else
-        #         res
-        #     end
-        # end
-
-        if (isstatic(method))
-            newmethod = :( $(Symbol(name))($(typed_parameters...)) = jcall(class, $name, $returntype, $parametertypes, $(parameters...)) )
-            Base.eval(mod, newmethod)
+    Base.getproperty(proxy::ImportProxy, sym::Symbol) = begin
+        if sym == :δmethods
+            getfield(proxy, :mod).static_methods
+        elseif sym == :δfields
+            getfield(proxy, :mod).static_fields
+        elseif sym == :δmod
+            getfield(proxy, :mod)
+        elseif sym == :class
+            wrapped(getfield(proxy, :mod).class_class)
+        elseif haskey(getfield(proxy, :mod).static_methods, sym)
+            eval(:( getfield($proxy, :mod).static_methods.$sym ))
+        elseif haskey(getfield(proxy, :mod).static_fields, sym)
+            eval(:( getfield($proxy, :mod).static_fields.$sym() )) # call the getter
         else
-            inst_param = :(inst::$(class))
-            newmethod = :( $(Symbol(name))($inst_param) = ($(typed_parameters...),) -> jcall(inst, $name, $returntype, $parametertypes, $(parameters...)))
-            Base.eval(mod, newmethod)
+            local classname = getfield(proxy, :mod).name
+            Base.error("class $(classname) has no static method/field $(string(sym))")
         end
     end
 
-    Base.eval(mod, :( final = Set() ))
-    for field in JavaCall.listfields(class)
-        name = JavaCall.getname(field)
-        union!(mod.final, [name])
-        type = JavaCall.jcall(field, "getType", JClass, ())
-        type = normalizeJavaType(type)
-
-        if isstatic(field)
-            # commented this out for testing getters/setters
-            # if isfinal(field)
-            #     val = JavaCall.jfield(class, name, type)
-            #     Base.eval(mod, :( $(Symbol(name)) = $(val) ))
-        # else
-                # TODO: use better setter than the boxed get/set
-                getter = :( $(Symbol(name))() = JavaCall.jfield($(class), $(name), $(type)) )
-                setter = :( $(Symbol(name))(val::$(type)) = JavaCall.jcall($(field), "set", JavaCall.jvoid, ($(type)), ) )
-
-                Base.eval(mod, getter)
-                Base.eval(mod, setter)
-            # end
+    Base.setproperty!(proxy::ImportProxy, sym::Symbol, v::Any) = begin
+        if haskey(getfield(proxy, :mod).static_fields, sym)
+            eval(:( getfield($proxy, :mod).$sym($v) )) # call the setter
         else
-            # TODO
-        end 
+            local classname = getfield(proxy, :mod).name
+            Base.error("class $(classname) has no static field $(string(sym))")
+        end
     end
-    ClassProxy(classname, mod)
+
+    struct InstanceProxy
+        ref::JavaCall.JavaObject
+        mod::Module
+    end
+
+    Base.show(io::IO, proxy::InstanceProxy) =
+        show(io, getfield(proxy, :ref))
+
+    Base.getproperty(proxy::InstanceProxy, sym::Symbol) = begin
+        local this = getfield(proxy, :ref)
+        if sym == :δmethods
+            getfield(proxy, :mod).instance_methods(this)
+        elseif sym == :δfields
+            getfield(proxy, :mod).instance_fields(this)
+        elseif haskey(getfield(proxy, :mod).instance_methods, sym)
+            eval(:( getfield($proxy, :mod).instance_methods.$sym($this) ))
+        elseif haskey(getfield(proxy, :mod).instance_fields, sym)
+            eval(:( getfield($proxy, :mod).instance_fields.$sym($this)() )) # call the getter
+        else
+            local classname = getfield(proxy, :mod).name
+            Base.error("class $(classname) has no instance method/field $(string(sym))")
+        end
+    end
+
+    Base.setproperty!(proxy::InstanceProxy, sym::Symbol, v::Any) = begin
+        local this = getfield(proxy, :ref)
+        if haskey(getfield(proxy, :mod).instance_fields, sym)
+            eval(:( getfield($proxy, :mod).instance_fields.$sym($this)(v) )) # call the setter
+        else
+            local classname = getfield(proxy, :mod).name
+            Base.error("class $(classname) has no instance field $(string(sym))")
+        end
+    end
+
+    mdf = @jimport java.lang.reflect.Modifier
+    isstatic(method::Union{JavaCall.JField,JavaCall.JMethod}) = begin
+        local modifiers = JavaCall.jcall(method, "getModifiers", JavaCall.jint, ())
+        Bool(JavaCall.jcall(mdf, "isStatic", JavaCall.jboolean, (JavaCall.jint,), modifiers))
+    end
+    isfinal(method::Union{JavaCall.JField,JavaCall.JMethod}) = begin
+        local modifiers = JavaCall.jcall(method, "getModifiers", JavaCall.jint, ())
+        Bool(JavaCall.jcall(mdf, "isFinal", JavaCall.jboolean, (JavaCall.jint,), modifiers))
+    end
+
+    normalizeJavaType(name::Union{Core.String,Base.SubString}) = begin
+        if name == "short" || name == "java.lang.Short"
+            JavaCall.jshort
+        elseif name == "int" || name == "java.lang.Integer"
+            JavaCall.jint
+        elseif name == "long" || name == "java.lang.Long"
+            JavaCall.jlong
+        elseif name == "byte" || name == "java.lang.Byte"
+            JavaCall.jbyte
+        elseif name == "char" || name == "java.lang.Char"
+            JavaCall.jchar
+        elseif name == "float" || name == "java.lang.Float"
+            JavaCall.jfloat
+        elseif name == "double" || name == "java.lang.Double"
+            JavaCall.jdouble
+        elseif name == "boolean" || name == "java.lang.Boolean"
+            JavaCall.jboolean
+        elseif name == "void" || name == "java.lang.Void"
+            JavaCall.jvoid
+        elseif (m = Base.match(r"[^\][]+((\[])+)", name); typeof(m) != Nothing)
+            local n = m.captures[1].ncodeunits ÷ 2
+            local name_len = length(name) - m.captures[1].ncodeunits
+            Array{normalizeJavaType(Base.SubString(name, 1:name_len)),n}
+        else
+            # TODO: wrapper? maybe?
+            JavaCall.JavaObject{Symbol(name)}
+        end
+    end
+
+    normalizeJavaType(x::JavaCall.JClass) = begin
+        normalizeJavaType(getname(x))
+    end
+
+    java_primitive_types = Core.Union{Int8, Int16, Int32, Int64, UInt16, Float32, Float64, Nothing}
+
+    unwrapped(x::InstanceProxy) = getfield(x, :ref)
+    unwrapped(x::Union{JavaCall.JavaObject, java_primitive_types}) = x
+    unwrapped(x::Array{T, N}) where {T, N} = map(unwrapped, x)
+    unwrapped(x::Bool) = UInt8(x) # kinda ugly, but nothing else is a UInt8
+    unwrapped(::Type{Bool}) = UInt8
+    unwrapped(t::Type{<: Union{JavaCall.JavaObject, java_primitive_types}}) = t
+    unwrapped(::Type{<: InstanceProxy}) = JavaCall.JavaObject
+    unwrapped(::Type{Array{T, N}}) where {T, N} = Array{unwrapped(T), N}
+
+    wrapped(x::Union{InstanceProxy, java_primitive_types, String}) = x
+    wrapped(x::JavaCall.JavaObject{C}) where {C} = begin
+        local classname = string(C)
+        local mod = getfield(javaImportClass(classname), :mod)
+
+        InstanceProxy(x, mod)
+    end
+    wrapped(x::Array{T, N}) where {T, N} = map(wrapped, x)
+    wrapped(x::UInt8) = Bool(x) # kinda ugly, but nothing else is a UInt8
+    wrapped(::Type{UInt8}) = Bool
+    wrapped(::Type{<: JavaCall.JavaObject}) = InstanceProxy
+    wrapped(t::Type{<: Union{InstanceProxy, java_primitive_types}}) = t
+    wrapped(::Type{Array{T, N}}) where {T, N} = Array{wrapped(T), N}
+
+    javaClassModuleName(classname::String) = "PAvaJavaInterop Class " * classname
+
+    function javaImportClass(classname::String)
+        if ! Base.isdefined(Main.JavaProxyUtils, Symbol(javaClassModuleName(classname)))
+            loadJavaClass(classname)
+        end
+
+        mod = eval(Symbol(javaClassModuleName(classname)))
+        ImportProxy(mod)
+    end
+
+    function loadJavaClass(classname::String)
+        local class = eval(JavaCall._jimport(classname))
+        local mod_name = javaClassModuleName(classname)
+
+        eval(:(
+            module $(Symbol(mod_name))
+                using JavaCall
+                using Main.JavaProxyUtils: wrapped, unwrapped
+
+                class_class = $(JavaCall.classforname(classname))
+                class_type = $class
+                name = $classname
+            end
+        ))
+        local mod = eval(Symbol(mod_name))
+        local inst_param = :(inst::$(class))
+
+        local instance_method_names = Set{String}()
+        local static_method_names = Set{String}()
+        for method in JavaCall.listmethods(class)
+            local name = getname(method)
+            local returntype = normalizeJavaType(getreturntype(method))
+            local parametertypes = Tuple(normalizeJavaType(p) for p in getparametertypes(method))
+            local parameternames = [:($(Symbol("p" * string(i)))) for i in 1:length(parametertypes)]
+            local typed_parameters = [:($(a[1])::$(wrapped(a[2]))) for a in zip(parameternames, parametertypes)]
+
+            local newmethod_def = if (isstatic(method))
+                push!(static_method_names, name);
+
+                quote
+                    function $(Symbol("sm_" * name))($(typed_parameters...))
+                        $(( :($pn = unwrapped($pn)) for pn in parameternames )...);
+
+                        local res = JavaCall.jcall($class, $name, $returntype, $parametertypes, $(parameternames...))
+                        wrapped(res)
+                    end
+                end
+            else
+                push!(instance_method_names, name);
+
+                # TODO: overloads are impossible
+                quote
+                    function $(Symbol("im_" * name))(inst::$class)
+                        ($(typed_parameters...),) -> begin
+                            $(( :($pn = unwrapped($pn)) for pn in parameternames )...);
+
+                            local res = JavaCall.jcall(inst, $name, $returntype, $parametertypes, $(parameternames...))
+                            wrapped(res)
+                        end
+                    end
+                end
+            end
+
+            Base.eval(mod, newmethod_def)
+        end
+
+        local static_field_names = Set{String}()
+        local instance_field_names = Set{String}()
+        for field in JavaCall.listfields(class)
+            local name = JavaCall.getname(field)
+            local type = normalizeJavaType(JavaCall.jcall(field, "getType", JClass, ()))
+
+            if isstatic(field)
+                # TODO: use better setter than the boxed get/set
+                local getter = :( $(Symbol("sf_" * name))() = wrapped(JavaCall.jfield($class, $name, $type)) )
+                Base.eval(mod, getter)
+
+                if !isfinal(field)
+                    local setter = :( $(Symbol("sf_" * name))(val::$(wrapped(type))) = JavaCall.jcall($field, "set", Nothing, ($type,), unwrapped(val)) )
+                    Base.eval(mod, setter)
+                end
+
+                push!(static_field_names, name)
+            else
+                # TODO: use better setter than the boxed get/set
+                local getter = :( $(Symbol("if_" * name))($inst_param) = wrapped(JavaCall.jfield($class, $name, $type)) )
+                Base.eval(mod, getter)
+
+                if !isfinal(field)
+                    local setter = quote
+                        function $(Symbol("if_" * name))($inst_param, val::$(wrapped(type)))
+                            JavaCall.jcall($field, "set", JavaCall.jvoid, ($type,), unwrapped(val))
+                        end
+                    end
+                    Base.eval(mod, mod)
+                end
+
+                push!(instance_field_names, name)
+            end
+        end
+
+        local instance_methods = (:($(Symbol(n)) = $(Symbol("im_" * n))) for n in instance_method_names)
+        local static_methods = (:($(Symbol(n)) = $(Symbol("sm_" * n))) for n in static_method_names)
+        local instance_fields = (:($(Symbol(n)) = $(Symbol("if_" * n))) for n in instance_field_names)
+        local static_fields = (:($(Symbol(n)) = $(Symbol("sf_" * n))) for n in static_field_names)
+
+        Base.eval(mod, quote
+            instance_methods = ($(instance_methods...),)
+            static_methods = ($(static_methods...),)
+            instance_fields = ($(instance_fields...),)
+            static_fields = ($(static_fields...),)
+        end)
+
+        Base.eval(mod, :( new() = $("TODO: should be a constructor") )) # get all constructors
+    end
+
+    Base.convert(::Type{JavaProxyUtils.InstanceProxy}, s::String) = wrapped(JavaCall.JString(s))
+    Base.convert(::Type{String}, x::JavaProxyUtils.InstanceProxy) = String(unwrapped(x))
 end
+##
 
 ##
-class = javaClassModule("java.time.LocalDate")
+class = javaImportClass("java.time.LocalDate")
 mod = getfield(class, :mod)
 
 ##
@@ -193,7 +283,7 @@ unique!(instancemethods)
 instancemethods_ = (;zip([Symbol(m) for m in instancemethods], [eval(Symbol("pava2_" * m)) for m in instancemethods])...)
 
 now = LocalDate.now()
-method = (plusDays = (jtld) -> (days) -> jcall(jtld, "plusDays", jlm, (jlong,), days),)
+    method = (plusDays = (jtld) -> (days) -> jcall(jtld, "plusDays", jlm, (jlong,), days),)
 
-a = JavaValue(now, instancemethods_)
-# 
+a = InstanceProxy(now, instancemethods_)
+#
